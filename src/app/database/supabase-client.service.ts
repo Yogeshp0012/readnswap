cf7a116bc410245c46e3543c041398d64ee15d70
Yogeshp0012 <yogeshp0012@gmail.com>
2024-06-13 22:00:56 +0530

    Supabase fix

--- src/app/database/supabase-client.service.ts
+++ src/app/database/supabase-client.service.ts
@@ -20,6 +20,21 @@ export class SupabaseClientService {
     }
 
     addNewsletterSubscriber(email: string) {
-        console.log(this.supabase.from('newsletter').insert({ email }));
+        return this.supabase
+            .from('newsletter')
+            .insert({ email })
+            .then((response) => {
+                if (response.error) {
+                    console.error('Error adding subscriber:', response.error);
+                    throw response.error;
+                } else {
+                    console.log('Subscriber added successfully:', response.data);
+                    return response.data;
+                }
+            })
+            .then(null, (error) => {
+                console.error('Error adding subscriber:', error);
+                throw error;
+            });
     }
 }
