df0c32a677dddae967df4b45241df9e764f4e8b6
Yogeshp0012 <yogeshp0012@gmail.com>
2024-06-15 17:10:49 +0530

    Add email client

--- src/app/newsletter/newsletter.component.ts
+++ src/app/newsletter/newsletter.component.ts
@@ -1,20 +1,23 @@
 import { Component, inject } from '@angular/core';
-import { SupabaseClientService } from '../database/supabase-client.service';
 import { FormsModule } from '@angular/forms';
+import { SupabaseClientService } from '../database/supabase-client.service';
+import { ResendService } from '../mail-client/resend.service';
 
 @Component({
   selector: 'app-newsletter',
   standalone: true,
   imports: [FormsModule],
-  providers: [SupabaseClientService],
+  providers: [SupabaseClientService, ResendService],
   templateUrl: './newsletter.component.html',
   styles: ``
 })
 export class NewsletterComponent {
     private dbClient = inject(SupabaseClientService);
+    private emailClient = inject(ResendService);
     email: string = '';
 
     addNewsLetter(){
         this.dbClient.addNewsletterSubscriber(this.email);
+        this.emailClient.sendNewsletterSubscribed(this.email);
     }
 }
