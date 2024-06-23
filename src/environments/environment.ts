bbd2885a74af25b5e59d5da9fabf8d9bd32227c4
Yogeshp0012 <vipanchisamindla786@gmail.com>
2024-06-23 10:05:48 +0530

    font awesome icons

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    SUPABASE_URL: '',
-    SUPABASE_ANON_KEY: '',
-    BASE_URL: ''
+    SUPABASE_URL: 'https://scmxwkbchlgxkknbjgov.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjbXh3a2JjaGxneGtrbmJqZ292Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgyOTA4MTUsImV4cCI6MjAzMzg2NjgxNX0.YuAWCMvZ761HNKqhIIa_Ciz8H1ahX8g_Ekbf3TRyziY',
+    BASE_URL: 'undefined',
 };
