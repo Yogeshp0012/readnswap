404885d4890125471bc3a0bb463e6be115355396
Yogeshp0012 <vipanchisamindla786@gmail.com>
2024-06-24 16:07:00 +0530

    stats

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,12 @@ import { PricingComponent } from '../pricing/pricing.component';
 import { NewsletterComponent } from '../newsletter/newsletter.component';
 import { HeroComponent } from '../hero/hero.component';
 import { FeaturesComponent } from '../features/features.component';
+import { StatsComponent } from '../stats/stats.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [RouterOutlet, FooterComponent, PricingComponent, NewsletterComponent, HeroComponent, FeaturesComponent],
+    imports: [RouterOutlet, FooterComponent, PricingComponent, NewsletterComponent, HeroComponent, FeaturesComponent,StatsComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
